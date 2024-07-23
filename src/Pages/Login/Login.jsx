6b2639ed986b12a16c5b42b50fd7c8a37669ef9b
For-testing <129156990+Murodillo-dev@users.noreply.github.com>
2024-07-23 21:38:00 +0500

    .

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import logo from "./images/Storefront.png";
+import discord from "../../Icons/discord.png";
+import instagram from "../../Icons/instagram.png";
+import twitter from "../../Icons/twitter.png";
+import youtube from "../../Icons/youtube.png";
 
 const Login = () => {
     return (
         <div className="w-full h-auto bg-[#2B2B2B] ">
-            <div className="flex justify-between items-center bg-[#2B2B2B] flex-wrap px-[50px] py-5">
+            <div className="flex justify-between items-center bg-[#2B2B2B] flex-wrap px-[40px] py-5">
                 <div className="flex-1">
                     <a className="btn btn-ghost text-xl">
                         <img src={logo} alt="" />
@@ -14,7 +18,7 @@ const Login = () => {
                     </a>
                 </div>
                 <div className="flex-none">
-                    <ul className="flex justify-start items-center gap-3 text-white font-[work-sans] font-[600] text-[16px] ">
+                    <ul className="flex justify-start items-center gap-3 text-white font-[work-sans] font-[600] text-[14px] ">
                         <li>
                             <a className="px-5 py-3 " href="">
                                 Marketplace
@@ -31,10 +35,10 @@ const Login = () => {
                             </a>
                         </li>
                         <li>
-                            <button className="flex gap-3 px-[30px] rounded-[20px] py-5 box-border bg-[#A259FF] hover:bg-[#A259FF] active:bg-[#A259FF]">
+                            <button className="flex gap-3 text-[14px] px-[30px] rounded-[20px] py-5 box-border bg-[#A259FF] hover:bg-[#A259FF] active:bg-[#A259FF]">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
-                                    className="h-6 w-6"
+                                    className="h-5 w-5"
                                     fill="none"
                                     viewBox="0 0 24 24"
                                     stroke="currentColor"
@@ -48,7 +52,6 @@ const Login = () => {
                                 </svg>
                                 Sign Up
                             </button>
-
                         </li>
                     </ul>
                 </div>
@@ -61,7 +64,8 @@ const Login = () => {
                         Create account
                     </h1>
                     <p className="font-[work-sans] text-[22px] text-white max-w-[460px] w-full mt-5 font-[400]">
-                        Welcome! enter your details and start creating, collecting and selling NFTs.
+                        Welcome! enter your details and start creating, collecting and
+                        selling NFTs.
                     </p>
 
                     <label className="  max-w-[330px] rounded-[20px] mt-[40px] w-full h-[45px] input input-bordered flex items-center gap-2">
@@ -69,9 +73,9 @@ const Login = () => {
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 16 16"
                             fill="currentColor"
-                            className="h-4 w-4 opacity-70">
-                            <path
-                                d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                            className="h-4 w-4 opacity-70"
+                        >
+                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
                         </svg>
                         <input type="text" className="grow" placeholder="Username" />
                     </label>
@@ -81,11 +85,10 @@ const Login = () => {
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 16 16"
                             fill="currentColor"
-                            className="h-4 w-4 opacity-70">
-                            <path
-                                d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                            <path
-                                d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                            className="h-4 w-4 opacity-70"
+                        >
+                            <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                            <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
                         </svg>
                         <input type="email" className="grow" placeholder="Email Address" />
                     </label>
@@ -95,11 +98,13 @@ const Login = () => {
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 16 16"
                             fill="currentColor"
-                            className="h-4 w-4 opacity-70">
+                            className="h-4 w-4 opacity-70"
+                        >
                             <path
                                 fillRule="evenodd"
                                 d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                                clipRule="evenodd" />
+                                clipRule="evenodd"
+                            />
                         </svg>
                         <input type="password" className="grow" placeholder="Password" />
                     </label>
@@ -109,57 +114,81 @@ const Login = () => {
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 16 16"
                             fill="currentColor"
-                            className="h-4 w-4 opacity-70">
+                            className="h-4 w-4 opacity-70"
+                        >
                             <path
                                 fillRule="evenodd"
                                 d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                                clipRule="evenodd" />
+                                clipRule="evenodd"
+                            />
                         </svg>
-                        <input type="password" className="grow" placeholder="Confirm Password" />
+                        <input
+                            type="password"
+                            className="grow"
+                            placeholder="Confirm Password"
+                        />
                     </label>
 
-                    <button className="font-[work-sans] text-[16px] max-w-[330px] rounded-[20px] mt-[15px] w-full h-[45px] bg-[#A259FF] hover:bg-[#A259FF] active:bg-[#A259FF] text-white">Create account</button>
-
+                    <button className="font-[work-sans] text-[16px] max-w-[330px] rounded-[20px] mt-[15px] w-full h-[45px] bg-[#A259FF] hover:bg-[#A259FF] active:bg-[#A259FF] text-white">
+                        Create account
+                    </button>
                 </div>
             </div>
 
             <div>
-                <footer className="footer bg-[#3B3B3B] text-base-content px-[115px] py-10">
+                <footer className="footer bg-[#3B3B3B]  text-base-content px-[115px] py-10">
                     <nav>
-                        <a className="flex items-center gap-3 text-xl">
+                        <a className="flex items-center gap-3 text-[20px]">
                             <img src={logo} alt="" />
-                            <span className="text-white font-[space-mono]">
+                            <span className="text-white font-semibold font-[space-mono]">
                                 NFT Marketplace
                             </span>
                         </a>
-                        <a className="link link-hover">NFT marketplace UI created <br /> with Anima for Figma.</a>
-                        <a className="link link-hover">Join our community</a>
+                        <a className="link link-hover mt-7">
+                            NFT marketplace UI created <br /> with Anima for Figma.
+                        </a>
+                        <a className="link link-hover mt-5">Join our community</a>
+                        <nav className="flex gap-[10px] mt-4">
+                            <a className="link link-hover">
+                                <img src={discord} alt="" />
+                            </a>
+                            <a className="link link-hover">
+                                <img src={instagram} alt="" />
+                            </a>
+                            <a className="link link-hover">
+                                <img src={twitter} alt="" />
+                            </a>
+                            <a className="link link-hover">
+                                <img src={youtube} alt="" />
+                            </a>
+                        </nav>
                     </nav>
-                    <nav>
-                        <h6 className="footer-title">Company</h6>
-                        <a className="link link-hover">About us</a>
-                        <a className="link link-hover">Contact</a>
-                        <a className="link link-hover">Jobs</a>
-                        <a className="link link-hover">Press kit</a>
+
+                    <nav className="mt-2">
+                        <a className="text-white font-[space-mono] font-semibold text-[20px]">
+                            Explore
+                        </a>
+                        <a className="link link-hover mt-5">Marketplace</a>
+                        <a className="link link-hover mt-5">Rankings</a>
+                        <a className="link link-hover mt-5">Connect a wallet</a>
                     </nav>
-                    <nav>
-                        <h6 className="footer-title">Legal</h6>
-                        <a className="link link-hover">Terms of use</a>
-                        <a className="link link-hover">Privacy policy</a>
-                        <a className="link link-hover">Cookie policy</a>
-                    </nav>
-                    <form>
-                        <h6 className="footer-title">Newsletter</h6>
-                        <fieldset className="form-control w-80">
-                            <label className="label">
-                                <span className="label-text">Enter your email address</span>
-                            </label>
-                            <div className="join">
+
+                    <form className="mt-2">
+                        <a className="text-white font-[space-mono] font-semibold text-[20px]">
+                            Join our weekly digest
+                        </a>
+                        <fieldset className="form-control max-w-80 w-full">
+                            <a className="label-text text-[#CCCCCC] text-[16px] mt-6 mb-5">
+                                Get exclusive promotions & updates straight to your inbox.
+                            </a>
+
+                            <div className="join ">
                                 <input
                                     type="text"
-                                    placeholder="username@site.com"
-                                    className="input input-bordered join-item" />
-                                <button className="btn btn-primary join-item">Subscribe</button>
+                                    placeholder="Enter your email here"
+                                    className="input input-bordered join-item rounded-[15px] h-[60px]"
+                                />
+                                <button className="btn h-[60px] rounded-[20px] bg-[#A259FF] hover:bg-[#A259FF] active:bg-[#A259FF] join-item font-[work-sans] text-white px-[50px] py-5">Subscribe</button>
                             </div>
                         </fieldset>
                     </form>
