53e38b0b7ff2ccdcc9ec0b6b8acd6069991fb98a
For-testing <129156990+Murodillo-dev@users.noreply.github.com>
2024-07-26 14:03:46 +0500

    .

---
+++ src/Pages/Home/Data.jsx
@@ -0,0 +1,49 @@
+import dog from './Images/dog.png'
+import cat from './Images/cat.png'
+import bear from './Images/bear.png'
+import fox from './Images/fox.png'
+
+import mashroom from './Images/mashroom.png'
+import mashroom1 from './Images/mashroom1.png'
+import mashroom2 from './Images/mashroom2.png'
+import head from './Images/head.png'
+
+import human from './Images/human.png'
+import human1 from './Images/human1.png'
+import human2 from './Images/human2.png'
+import robot from './Images/robot.png'
+
+export const trending = [
+    {
+        id: 1,
+        mainImg: dog,
+        secondImg: cat,
+        thirdImg: bear,
+        title: 'DSGN Animals',
+        titleImg: fox,
+        statistic: 1025,
+        titleImgName: 'Mr Fox'
+    },
+    {
+        id: 2,
+        mainImg: mashroom,
+        secondImg: mashroom1,
+        thirdImg: mashroom2,
+        statistic: 1025,
+        title: 'Magic Mushrooms',
+        titleImg: head,
+        titleImgName: 'Shroomie'
+    }, {
+        id: 3,
+        mainImg: human,
+        secondImg: human1,
+        thirdImg: human2,
+        statistic: 1025,
+        title: 'Disco Machines',
+        titleImg: robot,
+        titleImgName: 'BeKind2Robots'
+    }
+]
+
+//================================================================================
+
