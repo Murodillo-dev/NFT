dc5e75f6550e43fc8181676f26b25115e5fd66a0
For-testing <129156990+Murodillo-dev@users.noreply.github.com>
2024-07-26 08:48:33 +0500

    .

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -8,29 +8,32 @@ import youtube from "../../Icons/youtube.png";
 const Login = () => {
     return (
         <div className="w-full h-auto bg-[#2B2B2B] ">
-            <div className="flex justify-between items-center bg-[#2B2B2B] flex-wrap px-[40px] py-5">
+
+            <div className="flex justify-between items-center bg-[#2B2B2B] flex-wrap px-[40px] py-5 max-ipad:px-5 max-other:justify-between max-mobile:p-2">
+
                 <div className="flex-1">
-                    <a className="btn btn-ghost text-xl">
-                        <img src={logo} alt="" />
+                    <a className="btn btn-ghost text-xl max-ipad:p-0 max-meduim:text-[16px] max-mobile:text-[14px]">
+                        <img src={logo} alt="" className="max-meduim:w-6 max-mobile:w-5" />
                         <span className="text-white font-[space-mono]">
                             NFT Marketplace
                         </span>
                     </a>
                 </div>
-                <div className="flex-none">
-                    <ul className="flex justify-start items-center gap-3 text-white font-[work-sans] font-[600] text-[14px] ">
+
+                <div className="flex-none max-other:hidden">
+                    <ul className="flex justify-start items-center gap-3 text-white font-[work-sans] font-[600] text-[14px] max-ipad:gap-0 ">
                         <li>
-                            <a className="px-5 py-3 " href="">
+                            <a className="px-5 py-3 max-ipad:px-3 py-3 " href="">
                                 Marketplace
                             </a>
                         </li>
                         <li>
-                            <a className="px-5 py-3 " href="">
+                            <a className="px-5 py-3 max-ipad:px-3 py-3 " href="">
                                 Rankings
                             </a>
                         </li>
                         <li>
-                            <a className="px-5 py-3 " href="">
+                            <a className="px-5 py-3 max-ipad:px-3 py-3 " href="">
                                 Connect a wallet
                             </a>
                         </li>
@@ -55,20 +58,73 @@ const Login = () => {
                         </li>
                     </ul>
                 </div>
+
+                <div className="drawer-end hidden max-other:block">
+                    <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+
+                    <div className="drawer-content">
+                        <label htmlFor="my-drawer-4" className="drawer-button">
+                            <i className="fa-solid fa-bars text-white text-[25px] max-meduim:text-[20px]"></i>
+                        </label>
+                    </div>
+
+                    <div className="drawer-side pt-4 ">
+                        <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
+                        <ul className="menu rounded-l-3xl bg-[#2b2b2bf9] text-white text-base-content min-h-full w-80 p-4 text-black font-[work-sans] font-[600] text-[14px] max-ipad:gap-0 max-meduim:w-[75%] ">
+                            <li>
+                                <a className="px-5 py-3 max-ipad:px-3 py-3 " href="">
+                                    Marketplace
+                                </a>
+                            </li>
+                            <li>
+                                <a className="px-5 py-3 max-ipad:px-3 py-3 " href="">
+                                    Rankings
+                                </a>
+                            </li>
+                            <li>
+                                <a className="px-5 py-3 max-ipad:px-3 py-3 " href="">
+                                    Connect a wallet
+                                </a>
+                            </li>
+                            <li>
+                                <button className="flex gap-3 text-[14px] px-[30px] rounded-[20px] py-5 box-border bg-[#A259FF] hover:bg-[#A259FF] active:bg-[#A259FF]">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-5 w-5"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M12 11c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"
+                                        />
+                                    </svg>
+                                    Sign Up
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
             </div>
 
-            <div className="create w-full min-h-[690px] h-full flex justify-between flex-wrap">
-                <div className="left w-[50%]"></div>
-                <div className="right w-[50%] h-full pl-14 py-[100px] box-border">
-                    <h1 className="font-[work-sans] text-[51px] text-white font-[600]">
+            <div className="create w-full min-h-[690px] h-full flex justify-between flex-wrap max-large:flex-col max-large:items-center">
+
+                <div className="left w-[50%] max-large:w-full max-large:h-[400px]"></div>
+
+                <div className="right w-[50%] h-full pl-14 py-[100px] box-border max-other:pl-8 pr-3 max-large:p-0 max-large:flex flex-col items-center max-large:py-6 max-large:text-center max-tablet:w-[100%] max-mobile:px-4">
+                    <h1 className="font-[work-sans] text-[51px] text-white font-[600] max-other:text-[40px] max-meduim:text-[36px] max-small:text-[25px]">
                         Create account
                     </h1>
-                    <p className="font-[work-sans] text-[22px] text-white max-w-[460px] w-full mt-5 font-[400]">
+                    <p className="font-[work-sans] text-[22px] text-white max-w-[460px] w-full mt-5 font-[400] max-other:text-[19px] max-meduim:text-[16px] max-small:text-[13px    ]">
                         Welcome! enter your details and start creating, collecting and
                         selling NFTs.
                     </p>
 
-                    <label className="  max-w-[330px] rounded-[20px] mt-[40px] w-full h-[45px] input input-bordered flex items-center gap-2">
+                    <label className=" max-w-[330px] rounded-[20px] mt-[40px] w-full h-[45px] input input-bordered flex items-center gap-2">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 16 16"
@@ -133,10 +189,11 @@ const Login = () => {
                         Create account
                     </button>
                 </div>
+
             </div>
 
             <div>
-                <footer className="footer bg-[#3B3B3B]  text-base-content px-[115px] py-10">
+                <footer className="footer bg-[#3B3B3B]  text-base-content px-[115px] py-10 max-ipad:px-5 flex justify-between max-other:flex-col max-meduim:">
                     <nav>
                         <a className="flex items-center gap-3 text-[20px]">
                             <img src={logo} alt="" />
@@ -182,18 +239,21 @@ const Login = () => {
                                 Get exclusive promotions & updates straight to your inbox.
                             </a>
 
-                            <div className="join ">
+                            <div className="join">
                                 <input
                                     type="text"
                                     placeholder="Enter your email here"
-                                    className="input input-bordered join-item rounded-[15px] h-[60px]"
+                                    className="input input-bordered join-item rounded-[15px] h-[60px] max-meduim:max-w-[250px] w-full"
                                 />
-                                <button className="btn h-[60px] rounded-[20px] bg-[#A259FF] hover:bg-[#A259FF] active:bg-[#A259FF] join-item font-[work-sans] text-white px-[50px] py-5">Subscribe</button>
+                                <div className="join">
+                                    <button className="btn h-[60px] rounded-[20px] bg-[#A259FF] hover:bg-[#A259FF] active:bg-[#A259FF] join-item font-[work-sans] text-white px-[50px] py-5 max-meduim:max-w-[50px] w-full">Subscribe</button>
+                                </div>
                             </div>
                         </fieldset>
                     </form>
                 </footer>
             </div>
+
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      'desktop': '1280px',
+      'laptop': '1024px',
+      'ipad': '992px',
+      'other': '820px',
+      'large':'768px',
+      'tablet': '640px',
+      'meduim': '576px',
+      'mobile': '400px',
+      'small':'320px'
+    },
   },
   plugins: [require('daisyui'),],
 }
