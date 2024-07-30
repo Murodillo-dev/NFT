dad8cb83dc95779a465563e5132f6b001aab5047
For-testing <129156990+Murodillo-dev@users.noreply.github.com>
2024-07-30 21:52:24 +0500

    .

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -55,11 +55,11 @@ const Home = () => {
                         data.map(value => {
                             return (
                                 <div className=' w-[30%]'>
-                                    <img className='w-full hover:scale-95 ease-in-out duration-200' src={value.mainImg} alt="" />
+                                    <img className='w-full hover:scale-95 ease-linear duration-200' src={value.mainImg} alt="" />
                                     <div className='flex justify-between mt-4'>
-                                        <div className='w-[30%] rounded-[20px] hover:scale-95 ease-in-out duration-200' style={{ background: `url(${value.secondImg})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center center', backgroundSize: 'cover' }}></div>
-                                        <div className='w-[30%] rounded-[20px] hover:scale-95 ease-in-out duration-200' style={{ background: `url(${value.thirdImg})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center center', backgroundSize: 'cover' }}></div>
-                                        <div className='w-[30%] h-[100px] hover:scale-95 ease-in-out duration-200 bg-[#A259FF] flex justify-center items-center text-[22px] text-white font-[work-sans] font-semibold rounded-[20px] '><h1>{value.statistic}+</h1></div>
+                                        <div className='w-[30%] rounded-[20px] hover:scale-95 ease-linear duration-200' style={{ background: `url(${value.secondImg})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center center', backgroundSize: 'cover' }}></div>
+                                        <div className='w-[30%] rounded-[20px] hover:scale-95 ease-linear duration-200' style={{ background: `url(${value.thirdImg})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center center', backgroundSize: 'cover' }}></div>
+                                        <div className='w-[30%] h-[100px] hover:scale-95 ease-linear duration-200 bg-[#A259FF] flex justify-center items-center text-[22px] text-white font-[work-sans] font-semibold rounded-[20px] '><h1>{value.statistic}+</h1></div>
                                     </div>
                                     <h2 className='text-white text-[22px] mt-[15px] font-[work-sans] font-semibold'>
                                         {value.title}
@@ -81,9 +81,10 @@ const Home = () => {
                 <h1 className='text-[38px] font-semibold'>Top creators</h1>
 
                 <div className='flex justify-between items-end mt-[-60px]'>
+                    
                     <h2 className='text-[22px] font-normal'>Checkout Top Rated Creators on the NFT Marketplace</h2>
 
-                    <button className='py-5 px-[50px]  border-2 border-[#A259FF] rounded-[20px] text-white text-[16px] font-[work-sans] font-semibold mt-7 flex justify-center items-center gap-3 hover:scale-90 ease-in-out duration-200'>
+                    <button className='py-5 px-[50px]  border-2 border-[#A259FF] rounded-[20px] text-white text-[16px] font-[work-sans] font-semibold mt-7 flex justify-center items-center gap-3 hover:scale-90 ease-linear duration-200'>
                         <svg className='w-5 h-5 ' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#ffffff" d="M156.6 384.9L125.7 354c-8.5-8.5-11.5-20.8-7.7-32.2c3-8.9 7-20.5 11.8-33.8L24 288c-8.6 0-16.6-4.6-20.9-12.1s-4.2-16.7 .2-24.1l52.5-88.5c13-21.9 36.5-35.3 61.9-35.3l82.3 0c2.4-4 4.8-7.7 7.2-11.3C289.1-4.1 411.1-8.1 483.9 5.3c11.6 2.1 20.6 11.2 22.8 22.8c13.4 72.9 9.3 194.8-111.4 276.7c-3.5 2.4-7.3 4.8-11.3 7.2l0 82.3c0 25.4-13.4 49-35.3 61.9l-88.5 52.5c-7.4 4.4-16.6 4.5-24.1 .2s-12.1-12.2-12.1-20.9l0-107.2c-14.1 4.9-26.4 8.9-35.7 11.9c-11.2 3.6-23.4 .5-31.8-7.8zM384 168a40 40 0 1 0 0-80 40 40 0 1 0 0 80z" /></svg>
                         Get Started
                     </button>
@@ -95,8 +96,13 @@ const Home = () => {
                     {
                         creator.map(value => {
                             return (
-                                <div className='text-white font-[work-sans] w-[22%] my-[15px] h-auto bg-[#3B3B3B] rounded-[20px] p-5'>
-
+                                <div className='text-white font-[work-sans] w-[22%] my-[15px] h-auto bg-[#3B3B3B] rounded-[20px] p-5 relative flex justify-center flex-wrap items-center hover:scale-95 ease-linear duration-200'>
+                                    <div className='w-[40px] h-[40px] bg-[#2B2B2B] rounded-[50%] absolute top-5 left-5 flex justify-center items-center'>
+                                        <h1 className='text-[16px] text-[#858584] font-normal font-[space-mono]'>{value.id}</h1>
+                                    </div>
+                                    <img className='w-[120p] h-[120px]' src={value.img} alt="" />
+                                    <h1 className='text-[22px] text-white font-semibold w-full text-center mt-5 font-[work-sans]'>{value.title}</h1>
+                                    <h2 className='text-[16px] font-normal text-white font-[space-mono] mt-2'> <span className='text-[#858584]'>Total Sales:</span> {value.price} ETH</h2>
                                 </div>
                             )
                         })
