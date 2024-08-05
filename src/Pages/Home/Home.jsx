f44d2154f7d51c12d4704f8e043f83922ac0066e
For-testing <129156990+Murodillo-dev@users.noreply.github.com>
2024-08-05 17:27:39 +0500

    .

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -16,59 +16,84 @@ const Home = () => {
     return (
         <div className='bg-[#2B2B2B] '>
 
-            <div className='py-20 px-[115px] flex justify-between'>
+            <div className='py-20 px-[115px] flex justify-between max-desktop:px-20 max-laptop:px-10 max-ipad:py-10 max-large:flex-col items-center max-meduim:px-5'>
 
-                <div className="max-w-[500px] w-[40% font-[work-sans]">
-                    <h1 className='text-white text-[67px] font-semibold leading-[70px] capitalize'>
+                <div className=" w-[50%] font-[work-sans] max-ipad:w-[100%] max-large:text-center">
+                    <h1 className='text-white text-[67px] font-semibold leading-[70px] capitalize max-laptop:text-[50px] max-ipad:text-[30px] max-ipad:leading-[40px]'>
                         Discover digital art & Collect NFTs
                     </h1>
-                    <p className='font-[work-sans] text-[22px] text-white leading-[35px] mt-5'>
+                    <p className='font-[work-sans] text-[22px] text-white leading-[35px] mt-5 max-ipad:text-[16px] max-ipad:leading-[25px] max-ipad:mb-10'>
                         NFT marketplace UI created with Anima for Figma. Collect, buy and sell art from more than 20k NFT artists.
                     </p>
-                    <button className='py-5 px-[50px] bg-[#A259FF] rounded-[20px] text-white text-[16px] font-[work-sans] font-semibold mt-7 flex justify-center items-center gap-3'>
+                    <button className='py-5 px-[50px] bg-[#A259FF] rounded-[20px] text-white text-[16px] font-[work-sans] font-semibold mt-7 flex justify-center items-center gap-3 max-large:hidden'>
                         <svg className='w-5 h-5' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#ffffff" d="M156.6 384.9L125.7 354c-8.5-8.5-11.5-20.8-7.7-32.2c3-8.9 7-20.5 11.8-33.8L24 288c-8.6 0-16.6-4.6-20.9-12.1s-4.2-16.7 .2-24.1l52.5-88.5c13-21.9 36.5-35.3 61.9-35.3l82.3 0c2.4-4 4.8-7.7 7.2-11.3C289.1-4.1 411.1-8.1 483.9 5.3c11.6 2.1 20.6 11.2 22.8 22.8c13.4 72.9 9.3 194.8-111.4 276.7c-3.5 2.4-7.3 4.8-11.3 7.2l0 82.3c0 25.4-13.4 49-35.3 61.9l-88.5 52.5c-7.4 4.4-16.6 4.5-24.1 .2s-12.1-12.2-12.1-20.9l0-107.2c-14.1 4.9-26.4 8.9-35.7 11.9c-11.2 3.6-23.4 .5-31.8-7.8zM384 168a40 40 0 1 0 0-80 40 40 0 1 0 0 80z" /></svg>
                         Get Started
                     </button>
 
-                    <div className="max-w-[510px] w-full flex justify-between mt-[30px] bg-transparent">
+                    <div className="max-w-[510px] w-full flex justify-between mt-[30px] bg-transparent max-large:hidden">
                         <div className="stat place-items-left p-0">
-                            <div className="stat-value text-[28px] text-white font-semibold">240K + </div>
-                            <div className="stat-desc text-[24px] font-[work-sans] text-white leading-10">Total Sale</div>
+                            <div className="stat-value text-[28px] text-white font-semibold max-ipad:text-[22px] ">240K + </div>
+                            <div className="stat-desc text-[24px] font-[work-sans] text-white leading-10 max-ipad:text-[18px]">Total Sale</div>
                         </div>
 
                         <div className="stat place-items-left p-0">
-                            <div className="stat-value text-[28px] text-white font-semibold">100K +</div>
-                            <div className="stat-desc text-[24px] font-[work-sans] text-white leading-10">Auctions</div>
+                            <div className="stat-value text-[28px] text-white font-semibold max-ipad:text-[22px]">100K +</div>
+                            <div className="stat-desc text-[24px] font-[work-sans] text-white leading-10 max-ipad:text-[18px]">Auctions</div>
                         </div>
 
                         <div className="stat place-items-left p-0">
-                            <div className="stat-value text-[28px] text-white font-semibold">200K +</div>
-                            <div className="stat-desc text-[24px] font-[work-sans] text-white leading-10">Artists</div>
+                            <div className="stat-value text-[28px] text-white font-semibold max-ipad:text-[22px]">200K +</div>
+                            <div className="stat-desc text-[24px] font-[work-sans] text-white leading-10 max-ipad:text-[18px]">Artists</div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="w-[45%] max-ipad:w-[100%]">
+                    <img className='w-[265%]' src="https://cdn.animaapp.com/projects/6357ce7c8a65b2f16659918c/files/heroanimationtransparentbck-2.gif" alt="" />
+                </div>
+
+                <button className='py-5 px-[50px] w-[50%] hidden bg-[#A259FF] rounded-[20px] text-white text-[16px] font-[work-sans] font-semibold mt-7  gap-3 max-large:block max-large:flex justify-center items-center max-meduim:w-[100%]'>
+                    <svg className='w-5 h-5' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#ffffff" d="M156.6 384.9L125.7 354c-8.5-8.5-11.5-20.8-7.7-32.2c3-8.9 7-20.5 11.8-33.8L24 288c-8.6 0-16.6-4.6-20.9-12.1s-4.2-16.7 .2-24.1l52.5-88.5c13-21.9 36.5-35.3 61.9-35.3l82.3 0c2.4-4 4.8-7.7 7.2-11.3C289.1-4.1 411.1-8.1 483.9 5.3c11.6 2.1 20.6 11.2 22.8 22.8c13.4 72.9 9.3 194.8-111.4 276.7c-3.5 2.4-7.3 4.8-11.3 7.2l0 82.3c0 25.4-13.4 49-35.3 61.9l-88.5 52.5c-7.4 4.4-16.6 4.5-24.1 .2s-12.1-12.2-12.1-20.9l0-107.2c-14.1 4.9-26.4 8.9-35.7 11.9c-11.2 3.6-23.4 .5-31.8-7.8zM384 168a40 40 0 1 0 0-80 40 40 0 1 0 0 80z" /></svg>
+                    Get Started
+                </button>
+
+                <div className="w-full hidden mt-[30px] bg-transparent max-large:block">
+                    <div className='flex justify-center'>
+                        <div className="stat place-items-left p-0 text-center">
+                            <div className="stat-value text-[28px] text-white font-semibold max-ipad:text-[22px] max-large:text-[28px] max-meduim:text-[22px]">240K + </div>
+                            <div className="stat-desc text-[24px] font-[work-sans] text-white leading-10 max-ipad:text-[18px] max-large:text-[20px] max-meduim:text-[16px]">Total Sale</div>
+                        </div>
+
+                        <div className="stat place-items-left p-0 text-center">
+                            <div className="stat-value text-[28px] text-white font-semibold max-ipad:text-[22px] max-large:text-[28px] max-meduim:text-[22px]">100K +</div>
+                            <div className="stat-desc text-[24px] font-[work-sans] text-white leading-10 max-ipad:text-[18px] max-large:text-[20px] max-meduim:text-[16px]">Auctions</div>
+                        </div>
+
+                        <div className="stat place-items-left p-0 text-center">
+                            <div className="stat-value text-[28px] text-white font-semibold max-ipad:text-[22px] max-large:text-[28px] max-meduim:text-[22px]">200K +</div>
+                            <div className="stat-desc text-[24px] font-[work-sans] text-white leading-10 max-ipad:text-[18px] max-large:text-[20px] max-meduim:text-[16px]">Artists</div>
                         </div>
                     </div>
                 </div>
 
-                <div className="w-[45%]">
-                    <img src="https://cdn.animaapp.com/projects/6357ce7c8a65b2f16659918c/files/heroanimationtransparentbck-2.gif" alt="" />
-                </div>
-
             </div>
 
-            <div className='py-20 px-[115px]'>
+            <div className='py-20 px-[115px] max-desktop:px-20 max-laptop:px-10 max-ipad:py-10 max-mobile:px-5'>
 
-                <h1 className='text-[38px] text-white font-[work-sans] font-semibold'>Trending Collection</h1>
-                <h3 className='text-[22px] text-[#FFFFFF] font-[work-sans]'>Checkout our weekly updated trending collection.</h3>
+                <h1 className='text-[38px] text-white font-[work-sans] font-semibold max-tablet:text-[30px] max-meduim:text-center'>Trending Collection</h1>
+                <h3 className='text-[22px] text-[#FFFFFF] font-[work-sans] max-tablet:text-[16px] max-meduim:text-center'>Checkout our weekly updated trending collection.</h3>
 
-                <div className="w-full flex justify-between flex-wrap gap-7 mt-[60px]">
+                <div className="w-full flex justify-between flex-wrap gap-7 mt-[60px] max-other:flex-col items-center">
                     {
                         data.map(value => {
                             return (
-                                <div className=' w-[30%]'>
+                                <div className=' w-[30%] max-other:w-[65%] max-tablet:w-[85%] max-mobile:w-[100%]'>
                                     <img className='w-full hover:scale-95 ease-linear duration-200' src={value.mainImg} alt="" />
                                     <div className='flex justify-between mt-4'>
-                                        <div className='w-[30%] rounded-[20px] hover:scale-95 ease-linear duration-200' style={{ background: `url(${value.secondImg})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center center', backgroundSize: 'cover' }}></div>
-                                        <div className='w-[30%] rounded-[20px] hover:scale-95 ease-linear duration-200' style={{ background: `url(${value.thirdImg})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center center', backgroundSize: 'cover' }}></div>
-                                        <div className='w-[30%] h-[100px] hover:scale-95 ease-linear duration-200 bg-[#A259FF] flex justify-center items-center text-[22px] text-white font-[work-sans] font-semibold rounded-[20px] '><h1>{value.statistic}+</h1></div>
+                                        <div className='w-[30%] rounded-[20px] hover:scale-95 ease-linear duration-200 max-other:h-[150px] max-meduim:h-[100px]' style={{ background: `url(${value.secondImg})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center center', backgroundSize: 'cover' }}></div>
+                                        <div className='w-[30%] rounded-[20px] hover:scale-95 ease-linear duration-200 max-other:h-[150px] max-meduim:h-[100px]' style={{ background: `url(${value.thirdImg})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center center', backgroundSize: 'cover' }}></div>
+                                        <div className='w-[30%] h-[100px] hover:scale-95 ease-linear duration-200 bg-[#A259FF] flex justify-center items-center text-[22px] text-white font-[work-sans] font-semibold rounded-[20px] max-other:h-[150px] max-meduim:h-[100px] '><h1>{value.statistic}+</h1></div>
                                     </div>
                                     <h2 className='text-white text-[22px] mt-[15px] font-[work-sans] font-semibold'>
                                         {value.title}
@@ -85,13 +110,13 @@ const Home = () => {
 
             </div>
 
-            <div className='py-20 px-[115px] font-[work-sans] text-white'>
+            <div className='py-20 px-[115px] font-[work-sans] text-white max-desktop:px-20 max-laptop:px-10'>
 
                 <h1 className='text-[38px] font-semibold'>Top creators</h1>
 
-                <div className='flex justify-between items-end mt-[-60px]'>
+                <div className='flex justify-between items-end mt-[-30px] max-other:flex-wrap max-other:mt-5'>
 
-                    <h2 className='text-[22px] font-normal'>Checkout Top Rated Creators on the NFT Marketplace</h2>
+                    <h2 className='text-[22px] font-normal max-ipad:max-w-[500px] max-other:max-w-[600px]'>Checkout Top Rated Creators on the NFT Marketplace</h2>
 
                     <button className='py-5 px-[50px]  border-2 border-[#A259FF] rounded-[20px] text-white text-[16px] font-[work-sans] font-semibold mt-7 flex justify-center items-center gap-3 hover:scale-95 ease-linear duration-200'>
                         <svg className='w-5 h-5 ' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#ffffff" d="M156.6 384.9L125.7 354c-8.5-8.5-11.5-20.8-7.7-32.2c3-8.9 7-20.5 11.8-33.8L24 288c-8.6 0-16.6-4.6-20.9-12.1s-4.2-16.7 .2-24.1l52.5-88.5c13-21.9 36.5-35.3 61.9-35.3l82.3 0c2.4-4 4.8-7.7 7.2-11.3C289.1-4.1 411.1-8.1 483.9 5.3c11.6 2.1 20.6 11.2 22.8 22.8c13.4 72.9 9.3 194.8-111.4 276.7c-3.5 2.4-7.3 4.8-11.3 7.2l0 82.3c0 25.4-13.4 49-35.3 61.9l-88.5 52.5c-7.4 4.4-16.6 4.5-24.1 .2s-12.1-12.2-12.1-20.9l0-107.2c-14.1 4.9-26.4 8.9-35.7 11.9c-11.2 3.6-23.4 .5-31.8-7.8zM384 168a40 40 0 1 0 0-80 40 40 0 1 0 0 80z" /></svg>
@@ -105,13 +130,13 @@ const Home = () => {
                     {
                         creator.map(value => {
                             return (
-                                <div className='text-white font-[work-sans] w-[23%] my-[15px] h-auto bg-[#3B3B3B] rounded-[20px] p-5 relative flex justify-center flex-wrap items-center hover:scale-95 ease-linear duration-200'>
+                                <div className='text-white font-[work-sans] w-[23%] my-[15px] h-auto bg-[#3B3B3B] rounded-[20px] p-5 relative flex justify-center flex-wrap items-center hover:scale-95 ease-linear duration-200 max-desktop:p-4 max-laptop:w-[30%]'>
                                     <div className='w-[40px] h-[40px] bg-[#2B2B2B] rounded-[50%] absolute top-5 left-5 flex justify-center items-center'>
                                         <h1 className='text-[16px] text-[#858584] font-normal font-[space-mono]'>{value.id}</h1>
                                     </div>
                                     <img className='w-[120p] h-[120px]' src={value.img} alt="" />
                                     <h1 className='text-[22px] text-white font-semibold w-full text-center mt-5 font-[work-sans]'>{value.title}</h1>
-                                    <h2 className='text-[16px] font-normal text-white font-[space-mono] mt-2'> <span className='text-[#858584]'>Total Sales:</span> {value.price} ETH</h2>
+                                    <h2 className='text-[16px] font-normal text-white font-[space-mono] mt-2 max-desktop:text-[14px]'> <span className='text-[#858584]'>Total Sales:</span> {value.price} ETH</h2>
                                 </div>
                             )
                         })
@@ -120,13 +145,13 @@ const Home = () => {
 
             </div>
 
-            <div className='py-20 px-[115px] font-[work-sans] text-white flex justify-between flex-wrap'>
+            <div className='py-20 px-[115px] font-[work-sans] text-white flex justify-between flex-wrap max-desktop:px-20 max-laptop:px-10'>
                 <h1 className='text-[38px] font-semibold w-full'>Browse Categories</h1>
 
                 {
                     categorie.map(value => {
                         return (
-                            <div className='w-[23%] h-auto mt-[30px] bg-[#3B3B3B] rounded-[20px] overflow-hidden hover:scale-95 ease-linear duration-200'>
+                            <div className='w-[23%] h-auto mt-[30px] bg-[#3B3B3B] rounded-[20px] overflow-hidden hover:scale-95 ease-linear duration-200 '>
                                 <div className='relative w-full h-60 flex justify-center items-center' style={{ background: `url(${value.img})`, backgroundPosition: 'center center', backgroundSize: 'cover' }}>
                                     <div className='absolute inset-0 backdrop-blur-[5px]'></div>
                                     <img className='relative z-[5]' src={value.icon} alt="" />
@@ -142,7 +167,7 @@ const Home = () => {
 
             </div>
 
-            <div className='py-20 px-[115px] font-[work-sans] text-white'>
+            <div className='py-20 px-[115px] font-[work-sans] text-white max-desktop:px-20 max-laptop:px-10'>
 
                 <h1 className='text-[38px] font-semibold'>Discover More NFTs</h1>
 
@@ -192,7 +217,7 @@ const Home = () => {
 
             </div>
 
-            <div className="highlight w-full h-[660px] flex justify-center items-center">
+            <div className="highlight w-full h-[660px] flex justify-center items-center max-desktop:px-20 max-laptop:px-10">
 
                 <div className='max-w-[300px] w-full p-[30px] rounded-[20px] text-center box-border  bg-[#3b3b3bd8] text-[35px] font-semibold font-[space-mono] text-white flex justify-between flex-wrap'>
 
@@ -224,7 +249,7 @@ const Home = () => {
 
             </div>
 
-            <div className='py-20 px-[155px] font-[work-sans] text-white'>
+            <div className='py-20 px-[155px] font-[work-sans] text-white max-desktop:px-20 max-laptop:px-10'>
 
                 <h1 className='text-[38px] font-semibold'>How it works</h1>
                 <p className='text-[22px] text-[#FFFFFF] font-normal mt-2'>Find out how to get started</p>
@@ -246,7 +271,7 @@ const Home = () => {
 
             </div>
 
-            <div className='py-20 px-[155px] font-[work-sans] text-white' >
+            <div className='py-20 px-[155px] font-[work-sans] text-white max-desktop:px-20 max-laptop:px-10' >
                 <div className='w-full h-auto p-[60px] bg-[#3B3B3B] rounded-[20px] flex justify-between flex-wrap'>
 
                     <div className='max-w-[425px] w-full h-[310px]' style={{ background: `url(${astranout})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center center', backgroundSize: 'cover' }}></div>
