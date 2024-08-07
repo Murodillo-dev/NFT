651f092d5aea646f8ecf7e0cb73d343e28d9c75f
For-testing <129156990+Murodillo-dev@users.noreply.github.com>
2024-08-08 01:33:16 +0500

    .

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -152,19 +152,19 @@ const Home = () => {
 
             </div>
 
-            <div className='py-20 px-[115px] font-[work-sans] text-white flex justify-between flex-wrap max-desktop:px-20 max-laptop:px-10 max-ipad:p-10 max-other:justify-around'>
+            <div className='py-20 px-[115px] font-[work-sans] text-white flex justify-between flex-wrap max-desktop:px-20 max-laptop:px-10 max-ipad:p-10 max-other:justify-around max-tablet:justify-between max-meduim:justify-between max-mobile:px-5 max-mobile:justify-center'>
                 <h1 className='text-[38px] font-semibold w-full max-meduim:text-center max-meduim:text-[30px]'>Browse Categories</h1>
 
                 {
                     categorie.map(value => {
                         return (
-                            <div className='w-[23%] h-auto mt-[30px] bg-[#3B3B3B] rounded-[20px] overflow-hidden hover:scale-95 ease-linear duration-200 max-other:w-[45%]'>
-                                <div className='relative w-full h-60 flex justify-center items-center max-other:h-[300px]' style={{ background: `url(${value.img})`, backgroundPosition: 'center center', backgroundSize: 'cover' }}>
+                            <div className='w-[23%] h-auto mt-[30px] bg-[#3B3B3B] rounded-[20px] overflow-hidden hover:scale-95 ease-linear duration-200 max-other:w-[45%] max-tablet:w-[48%] max-meduim:w-[47%] max-mobile:w-[90%]'>
+                                <div className='relative w-full h-60 flex justify-center items-center max-other:h-[300px] max-meduim:h-[200px] max-mobile:h-72' style={{ background: `url(${value.img})`, backgroundPosition: 'center center', backgroundSize: 'cover' }}>
                                     <div className='absolute inset-0 backdrop-blur-[5px]'></div>
                                     <img className='relative z-[5]' src={value.icon} alt="" />
                                 </div>
-                                <div className='py-5 px-7'>
-                                    <h2 className='text-[22px] font-semibold max-ipad:text-[16px] max-other:text-[20px]'>{value.title}</h2>
+                                <div className='py-5 px-7 max-meduim:px-4'>
+                                    <h2 className='text-[22px] font-semibold max-ipad:text-[16px] max-other:text-[20px] max-meduim:text-[18px] '>{value.title}</h2>
                                 </div>
                             </div>
 
@@ -174,36 +174,35 @@ const Home = () => {
 
             </div>
 
-            <div className='py-20 px-[115px] font-[work-sans] text-white max-desktop:px-20 max-laptop:px-10 '>
+            <div className='py-20 px-[115px] font-[work-sans] text-white max-desktop:px-20 max-laptop:px-10 max-ipad:p-10'>
 
-                <h1 className='text-[38px] font-semibold'>Discover More NFTs</h1>
+                <h1 className='text-[38px] font-semibold max-meduim:text-[30px] text-center'>Discover More NFTs</h1>
 
-                <div className='flex justify-between items-end mt-[-60px]'>
+                <div className='flex justify-between items-end mt-[-30px] max-other:flex-wrap max-other:mt-5'>
 
-                    <h2 className='text-[22px] font-normal'>Explore new trending NFTs</h2>
+                    <h2 className='text-[22px] font-normal max-ipad:max-w-[500px] max-other:max-w-[600px] max-meduim:text-center max-meduim:w-full max-meduim:text-[16px]'>Checkout Top Rated Creators on the NFT Marketplace</h2>
 
-                    <button className='py-5 px-[50px]  border-2 border-[#A259FF] rounded-[20px] text-white text-[16px] font-[work-sans] font-semibold mt-7 flex justify-center items-center gap-3 hover:scale-95 ease-linear duration-200'>
+                    <button className='py-5 px-[50px]  border-2 border-[#A259FF] rounded-[20px] text-white text-[16px] font-[work-sans] font-semibold mt-7 flex justify-center items-center gap-3 hover:scale-95 ease-linear duration-200 max-tablet:hidden'>
                         <svg className='w-5 h-5 ' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#ffffff" d="M156.6 384.9L125.7 354c-8.5-8.5-11.5-20.8-7.7-32.2c3-8.9 7-20.5 11.8-33.8L24 288c-8.6 0-16.6-4.6-20.9-12.1s-4.2-16.7 .2-24.1l52.5-88.5c13-21.9 36.5-35.3 61.9-35.3l82.3 0c2.4-4 4.8-7.7 7.2-11.3C289.1-4.1 411.1-8.1 483.9 5.3c11.6 2.1 20.6 11.2 22.8 22.8c13.4 72.9 9.3 194.8-111.4 276.7c-3.5 2.4-7.3 4.8-11.3 7.2l0 82.3c0 25.4-13.4 49-35.3 61.9l-88.5 52.5c-7.4 4.4-16.6 4.5-24.1 .2s-12.1-12.2-12.1-20.9l0-107.2c-14.1 4.9-26.4 8.9-35.7 11.9c-11.2 3.6-23.4 .5-31.8-7.8zM384 168a40 40 0 1 0 0-80 40 40 0 1 0 0 80z" /></svg>
                         View Rankings
                     </button>
 
-
                 </div>
 
-                <div className='w-full flex justify-between mt-[60px]'>
+                <div className='w-full flex justify-between flex-wrap mt-[60px] max-tablet:justify-center max-tablet:mt-[30px]'>
                     {
                         discovers.map(value => {
                             return (
-                                <div className='w-[30%] h-auto bg-[#3B3B3B] font-[work-sans] text-white rounded-[20px] overflow-hidden hover:scale-95 ease-linear duration-200'>
-                                    <div className='w-full h-[300px]' style={{ background: `url(${value.image})`, backgroundPosition: 'center center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}></div>
-                                    <div className='py-5 px-[30px]'>
-                                        <h1 className='text-[22px] font-semibold'>{value.title}</h1>
+                                <div className='w-[30%] h-auto bg-[#3B3B3B] font-[work-sans] text-white rounded-[20px] overflow-hidden hover:scale-95 ease-linear duration-200 max-ipad:w-[32%] max-tablet:w-[70%] max-tablet:mt-[30px] max-meduim:w-[90%]'>
+                                    <div className='w-full h-[300px] max-other:h-56 max-tablet:h-[320px]' style={{ background: `url(${value.image})`, backgroundPosition: 'center center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}></div>
+                                    <div className='py-5 px-[30px] max-other:px-5'>
+                                        <h1 className='text-[22px] font-semibold max-other:text-[18px]'>{value.title}</h1>
                                         <div className=' w-full font-[space-mono] flex justify-start gap-3 mt-[5px]'>
                                             <img src={value.smallImg} alt="" />
-                                            <h2>{value.name}</h2>
+                                            <h2 className='max-other:text-[14px]'>{value.name}</h2>
                                         </div>
-                                        <div className='font-[space-mono] mt-6 flex justify-between'>
-                                            <h1>
+                                        <div className='font-[space-mono] mt-6 flex justify-between max-other:flex-col'>
+                                            <h1 className='max-other:border-b-2 pb-[2px]'>
                                                 <span className='text-[#858584] text-[12px] font-normal'>Price</span>
                                                 <br />
                                                 {value.price} ETH
@@ -220,6 +219,12 @@ const Home = () => {
                             )
                         })
                     }
+
+                    <button className='hidden py-5 px-7 border-2 border-[#A259FF] rounded-[20px] text-white text-[16px] font-[work-sans] font-semibold mt-7 hover:scale-95 ease-linear duration-200 max-tablet:block max-tablet:flex justify-center items-center gap-3 max-tablet:w-[75%] max-meduim:w-[90%] max-mobile:w-[100%]'>
+                        <svg className='w-5 h-5' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#ffffff" d="M156.6 384.9L125.7 354c-8.5-8.5-11.5-20.8-7.7-32.2c3-8.9 7-20.5 11.8-33.8L24 288c-8.6 0-16.6-4.6-20.9-12.1s-4.2-16.7 .2-24.1l52.5-88.5c13-21.9 36.5-35.3 61.9-35.3l82.3 0c2.4-4 4.8-7.7 7.2-11.3C289.1-4.1 411.1-8.1 483.9 5.3c11.6 2.1 20.6 11.2 22.8 22.8c13.4 72.9 9.3 194.8-111.4 276.7c-3.5 2.4-7.3 4.8-11.3 7.2l0 82.3c0 25.4-13.4 49-35.3 61.9l-88.5 52.5c-7.4 4.4-16.6 4.5-24.1 .2s-12.1-12.2-12.1-20.9l0-107.2c-14.1 4.9-26.4 8.9-35.7 11.9c-11.2 3.6-23.4 .5-31.8-7.8zM384 168a40 40 0 1 0 0-80 40 40 0 1 0 0 80z" /></svg>
+                        View Rankings
+                    </button>
+
                 </div>
 
             </div>
